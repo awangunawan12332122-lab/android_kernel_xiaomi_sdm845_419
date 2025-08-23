3fcdd53e6aaacb4c14360b7fefa9b948d7b81f48
duckyduckG <duckyduckg65@gmail.com>
2025-08-23 14:32:42 +0500

    drivers: gpu: Import xiaomi changes
    * from dipper-q-oss

--- drivers/gpu/drm/drm_ioctl.c
+++ drivers/gpu/drm/drm_ioctl.c
@@ -482,6 +482,28 @@ static int drm_copy_field(char __user *buf, size_t *buf_len, const char *value)
 	return 0;
 }
 
+#define MAX_TASK_NAME_LEN 30
+#define MAX_LIST_NUM 4
+char support_list[MAX_LIST_NUM][MAX_TASK_NAME_LEN] = {
+		"displayfeature",
+		"DisplayFeature",
+		"disp_pcc"
+		"displayeffect"
+};
+
+static bool drm_master_filter(char *task_name)
+{
+	unsigned int i = 0;
+	bool ret = false;
+	for (i=0; i<MAX_LIST_NUM; i++) {
+		if (!strncmp(task_name, support_list[i], strlen(support_list[i]))) {
+			ret = true;
+			break;
+		}
+	}
+	return ret;
+}
+
 /*
  * Get version information
  *
@@ -528,6 +550,7 @@ int drm_version(struct drm_device *dev, void *data,
  */
 int drm_ioctl_permit(u32 flags, struct drm_file *file_priv)
 {
+	struct task_struct *task = get_current();
 	/* ROOT_ONLY is only for CAP_SYS_ADMIN */
 	if (unlikely((flags & DRM_ROOT_ONLY) && !capable(CAP_SYS_ADMIN)))
 		return -EACCES;
@@ -539,8 +562,11 @@ int drm_ioctl_permit(u32 flags, struct drm_file *file_priv)
 
 	/* MASTER is only for master or control clients */
 	if (unlikely((flags & DRM_MASTER) &&
-		     !drm_is_current_master(file_priv)))
-		return -EACCES;
+		     !drm_is_current_master(file_priv))) {
+		if (!drm_master_filter(task->comm)) {
+			return -EACCES;
+		}
+	}
 
 	/* Render clients must be explicitly allowed */
 	if (unlikely(!(flags & DRM_RENDER_ALLOW) &&
