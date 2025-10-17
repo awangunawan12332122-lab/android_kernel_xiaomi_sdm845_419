97db53e3bbc24ccce1e843fb6b0587a58af75611
duckyduckG <duckyduckg65@gmail.com>
2025-10-18 02:00:57 +0500

    drivers: irqchip: Import xiaomi changes
    * from dipper-q-oss

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -727,6 +727,12 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 			name = desc->action->name;
 
 		pr_warn("%s: %d triggered %s\n", __func__, irq, name);
+
+		if (irq == 15)
+			continue;
+		if (irq == 414)
+			continue;
+
 	}
 }
 
